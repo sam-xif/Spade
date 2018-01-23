d26ffaf69fb20346ca07b500785bc21ed955bd2b
sam-xif <s.xifaras999@gmail.com>
2018-01-23 10:56:23 -0500

    Added notes.txt and Comment and Selection classes

---
+++ src/comment.py
@@ -0,0 +1,10 @@
+"""
+comment.py
+
+Comment class
+"""
+
+class Comment:
+    def __init__(self, selection, content):
+        self.selection = selection
+        self.content = content
---
+++ src/selection.py
@@ -0,0 +1,11 @@
+"""
+selection.py
+
+Selection class
+"""
+
+class Selection:
+    def __init__(self, url, location, text):
+        self.url = url
+        self.location = location
+        self.text = text
