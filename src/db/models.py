3d967c1a389c89aee8796ff66b2b0bc3990f4a5a
sam-xif <s.xifaras999@gmail.com>
2018-01-25 10:08:27 -0500

    Moved the models into models.py

--- src/db/models.py
+++ src/db/models.py
@@ -1,3 +1,26 @@
 """
 models.py
+
+This file should contain a reflection of the database schema.
+The classes are linked to tables in the database with the sqlalchemy Declarative API.
+
+If a change to this schema is made, it is important to follow these steps to perform a migration of the database:
+1. Run the command, replacing <name> with a short description of the upgrade: python make_migration.py "<name>"
+2. Run the command: python manage.py upgrade
+
+Note:
+Only use single line comments in this file for generate_pymodels.py to work correctly
 """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import *
+Base = declarative_base()
+
+
+class Comment:
+    __tablename__ = 'comments'
+    
+    ID = Column(Integer, primary_key=True)
+    selection = Column(String)
+    content = Column(String)
