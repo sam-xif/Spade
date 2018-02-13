925a386557f6abd6f1d97aeaa7cb6a59ee45ae31
Jocelyn Shen <jocelyn.j.shen@gmail.com>
2018-02-13 11:50:15 -0500

    cleaning tweets

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,16 +1,14 @@
 #Twitter Scrape
-
 import tweepy
 from nltk.stem.porter import PorterStemmer
 import re
 from collections import Counter
 import pandas as pd
+from src.processing.index import Index
 
 def get_api():
-    c_key = input("Consumer key: ")
-    c_secret = input("Consumer secret: ")
-    a_key = input("Access token: ")
-    a_secret = input("Access token secret: ")
+    a = open('keys.txt')
+    c_key, c_secret, a_key, a_secret = a.read().split("\n")[0:4]
     auth = tweepy.OAuthHandler(c_key, c_secret, "https://google.com")
     auth.set_access_token(a_key, a_secret)
     api = tweepy.API(auth)
@@ -21,7 +19,17 @@ def get_tweet_stream(keyword):
     for i in tweepy.Cursor(get_api().search, q=keyword).items(25):
         tweets.append(i.text.encode("utf-8"))
     df = pd.DataFrame(tweets, columns = ['Tweet'])
-    df.to_csv("twitter_data.csv")
+    df = clean(df)
     return df
+
+def clean(df):
+    a = Index()
+    tweet_contents = df['Tweet'].values
+    tweet_contents = [str(x.decode('utf-8')) for x in tweet_contents]
+    print(tweet_contents)
+    tweets_cleaned = a.preprocess(tweet_contents)                      #Fix str/byte error
+    df_=pd.DataFrame(tweets_cleaned, columns = ['Tweet'])
+    df_.to_csv("twitter_data.csv")
+    return df_
 
 print(get_tweet_stream('dog'))
