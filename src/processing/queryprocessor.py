7ef9106a91ee996e1ebe9029994e0e170df9bb58
Sam Xifaras <s.xifaras999@gmail.com>
2018-02-22 10:04:47 -0500

    Added queryprocessor.py

---
+++ src/processing/queryprocessor.py
@@ -0,0 +1,16 @@
+"""
+queryprocessor.py
+"""
+
+from src.processing.lsa import LSA
+from src.processing.index import Index
+
+class QueryProcessor:
+    def __init__(self):
+        pass
+
+    def build_from_db(self, dbcontroller):
+        pass
+
+    def query(self, query_string):
+        pass
